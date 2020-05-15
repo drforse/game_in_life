d80dabfeb675d00fe9ab5c5233da20c3204c2391
dr_forse <george.lifeslice@gmail.com>
2020-05-15 21:57:08 +0100

    fix /action with self

--- bot/user_commands/action.py
+++ bot/user_commands/action.py
@@ -10,7 +10,7 @@ class Action(BaseAction):
     @classmethod
     async def execute(cls, m: Message):
         if not re.match(r'/action(@.+)? .+ (\| .+ \| [1-9][0-9]*)+ \| .+$', m.text):
-            await m.answer('Команда не следует шаблону "/action действие | сообщение | ожидание | сообщение...\n'
+            await m.answer('Команда не следует шаблону "/action действие | сообщение | ожидание | сообщение..."\n'
                            'Пример: /action сходить в кафе | {me} и {reply} пошли в кафе | 5 | {me} и {reply} пьют '
                            'кофе | 0 | {me} обняла {reply} и они разошлись по домам')
             return
@@ -20,18 +20,18 @@ class Action(BaseAction):
         except:
             pass
 
+        action = m.text.split('|')[0].split(maxsplit=1)[1].strip()
+        async with cls.dp.current_state(chat=m.chat.id, user=m.from_user.id).proxy() as dt:
+            dt['action'] = action
+            print(m.text.split('|', maxsplit=1)[1].strip())
+            dt['messages_and_delays'] = m.text.split('|', maxsplit=1)[1].strip()
+
         player = Player(tg_id=m.from_user.id)
         if m.from_user.id != m.reply_to_message.from_user.id:
             second_player = Player(tg_id=m.reply_to_message.from_user.id)
         else:
             await cls.accept_action(data=f'action custom accept {player.tg_id} {player.tg_id}', message=m)
             return
-
-        action = m.text.split('|')[0].split(maxsplit=1)[1].strip()
-        async with cls.dp.current_state(chat=m.chat.id, user=m.from_user.id).proxy() as dt:
-            dt['action'] = action
-            print(m.text.split('|', maxsplit=1)[1].strip())
-            dt['messages_and_delays'] = m.text.split('|', maxsplit=1)[1].strip()
 
         kb = InlineKeyboardMarkup()
         accept = InlineKeyboardButton('Го', callback_data=f'action custom accept {player.tg_id} {second_player.tg_id}')
