91e77aff9569ad0b190a959a5acd290155bb11e1
dr_forse <george.lifeslice@gmail.com>
2020-07-01 20:55:25 +0100

    add limit name to not have mention and text_mention entities

--- bot/game/main.py
+++ bot/game/main.py
@@ -39,6 +39,10 @@ class Game:
         if len(m.text) > 50:
             await m.answer('Имя должно быть меньше пятидесяти символов')
             return
+        for entity in m.entities:
+            if entity.type in ['mention', 'text_mention']:
+                await m.answer('Имя не может содержать теги')
+                return
         async with state.proxy() as dt:
             dt['name'] = m.html_text
         kb = ReplyKeyboardMarkup()
