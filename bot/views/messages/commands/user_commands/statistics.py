ade8671454d0fa329150447c435ab03bc031fdd5
drforse <george.lifeslice@gmail.com>
2020-07-17 22:39:25 +0100

    add /statistics

---
+++ bot/views/messages/commands/user_commands/statistics.py
@@ -0,0 +1,18 @@
+from aiogram.types import Message
+
+from bot.views.base import UserCommandView
+from bot.game.statistics import *
+
+
+class Statistics(UserCommandView):
+    state = lambda: '*'
+    needs_auth = False
+    needs_reply_auth = False
+    needs_satiety_level = 0
+
+    @classmethod
+    async def execute(cls, m: Message, state=None):
+        users = await get_all_users()
+        chats = await get_all_chats()
+        text = 'Всего игроков: %s\nВсего стран: %s' % (len(users), len(chats))
+        await m.answer(text)
