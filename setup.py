e158c3ca203f2ff30d5cb8ba4eb325c07ea53724
drforse <george.lifeslice@gmail.com>
2020-08-24 21:34:53 +0100

    add setup.py to make package installable :)

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="game_in_life_bot",
+    version="0.0.1",
+    author="drforse",
+    author_email="george.lifeslice@gmail.com",
+    description="@gameinlife_bot",
+    long_description="https://t.me/gameinlife_bot",
+    long_description_content_type="text/markdown",
+    url="https://github.com/drforse/hastebin_bot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
