09ad5aa073f76bd7429e4d66ba1123d14d15433c
George Imedashvili <george.lifeslice@gmail.com>
2020-12-23 06:32:47 +0000

    Prevent no event loop error

--- game_in_life_bot/config.py
+++ game_in_life_bot/config.py
@@ -2,14 +2,16 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 from aiogram import Bot
 import os
+import asyncio
 
 API_TOKEN = os.environ['game_in_life_bot_token']
 DB_URL = os.environ['game_in_life_db_url']
 SENDERMAN_SECURE_API_BOT_TOKEN = os.environ['senderman_secure_api_bot_token']
 
 storage = MemoryStorage()
-bot = Bot(API_TOKEN, parse_mode='html')
-dp = Dispatcher(bot, storage=storage)
+loop = asyncio.get_event_loop() or asyncio.new_event_loop()
+bot = Bot(API_TOKEN, parse_mode='html', loop=loop)
+dp = Dispatcher(bot, storage=storage, loop=loop)
 
 
 GAME_SPEED = 21600  # seconds == 1 age
