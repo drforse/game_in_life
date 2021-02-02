05d2a7de86b24e87bde19b2ac8b3ed584e790e1d
drforse <george.lifeslice@gmail.com>
2021-02-02 20:56:46 +0000

    change setattr, add getattr, add all user's profile photos to ged_default_user_pics

--- game_in_life_bot/bot/views/inline/get_default_user_pics.py
+++ game_in_life_bot/bot/views/inline/get_default_user_pics.py
@@ -12,8 +12,12 @@ class GetDefaultUserPics(InlineQueryView):
     @classmethod
     async def execute(cls, q: InlineQuery, state: FSMContext):
         default_user_photos = []
-        for num, photo_id in enumerate(DefaultUserpicsModel.get().photo_ids):
+        photo_ids = DefaultUserpicsModel.get().photo_ids.copy()
+        user_profile_photos = await q.from_user.get_profile_photos()
+        photo_ids += [ph[-1].file_id for ph in user_profile_photos.photos]
+        for num, photo_id in enumerate(photo_ids):
             photo = InlineQueryResultCachedPhoto(id=str(num),
                                                  photo_file_id=photo_id)
             default_user_photos.append(photo)
+
         await q.answer(default_user_photos)
--- game_in_life_bot/bot/views/messages/commands/dev_commands/getattr.py
+++ game_in_life_bot/bot/views/messages/commands/dev_commands/getattr.py
@@ -4,7 +4,7 @@ from ......bot.views.base import DevCommandView
 from ......models import UserModel
 
 
-class SetAttr(DevCommandView):
+class GetAttr(DevCommandView):
 
     @staticmethod
     async def execute(m: Message):
@@ -13,12 +13,6 @@ class SetAttr(DevCommandView):
         args = m.text.split()[1:]
         user_model = UserModel.get(tg_id=m.reply_to_message.from_user.id)
         if user_model:
-            try:
-                value = eval(args[1])
-            except (NameError, SyntaxError):
-                value = args[1]
-            setattr(user_model, args[0], value)
-            user_model.save()
-            await m.answer("Success")
+            await m.answer(str(getattr(user_model, args[0])))
         else:
             await m.answer("User not found")
